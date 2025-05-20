fd88bb5a8cc07a24ea4b4932ea8c4a8b08d231b6
Sivaprakash <prakashboy99@gmail.com>
2025-05-20 18:42:17 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+import Avatar from "./Components/Avatar";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Avatar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/configs/Speakers.tsx
@@ -0,0 +1,7 @@
+export const Speakers = [
+  { value: "en-GB-BellaNeural ", label: "English" },
+  { value: "ta-SG-AnbuNeural", label: "Tamil (Singapore)" },
+  { value: "ta-IN-PallaviNeural", label: "Tamil (India)" },
+];
+
+export default Speakers;
